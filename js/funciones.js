197827807832c51a48c34eb35438f142c47adcbb
Yhuver <yhuver@gmail.com>
2018-11-27 15:41:36 -0500

    Se agregaron las vistas ya hechas
    Sobre estas vistas se inciara el proyecto

---
+++ js/funciones.js
@@ -0,0 +1,20 @@
+function opcion1() {
+	var a;
+	a=document.getElementById("informacion");
+	a.innerHTML='<object type="text/html" data="html/quienessomos.html" width="100%" height="100%"></object>';
+	}
+function opcion2() {
+	var a;
+	a=document.getElementById("informacion");
+	a.innerHTML='<object type="text/html" data="html/trailer.html" width="100%" height="100%"></object>';
+	}
+function opcion3() {
+	var a;
+	a=document.getElementById("informacion");
+	a.innerHTML='<object type="text/html" data="html/noticias.html" width="100%" height="100%"></object>';
+	}
+function opcion4() {
+	var a;
+	a=document.getElementById("informacion");
+	a.innerHTML='<object type="text/html" data="html/contacto.html" width="100%" height="100%"></object>';
+	}
