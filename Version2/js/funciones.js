63341a69c6fca31e9d8f77c1251ba9a863513fc5
Yhuver <yhuver@gmail.com>
2018-11-30 07:14:19 -0500

    Terminadas
    Solo faltaria arreglar los textos de contacto, nosotros y trailer

--- Version2/js/funciones.js
+++ Version2/js/funciones.js
@@ -8,12 +8,17 @@ function opcionIniciarSesion() {
 	a=document.getElementById("formularios");
 	a.innerHTML='<object type="text/html" data="html/iniciarsesion.html" width="100%" height="100%"></object>';
 	}
-function opcion3() {
+function opcionNosotros() {
 	var a;
 	a=document.getElementById("informacion");
-	a.innerHTML='<object type="text/html" data="html/noticias.html" width="100%" height="100%"></object>';
+	a.innerHTML='<object type="text/html" data="html/nosotros.html" width="100%" height="100%"></object>';
 	}
-function opcion4() {
+function opcionTrailer() {
+	var a;
+	a=document.getElementById("informacion");
+	a.innerHTML='<object type="text/html" data="html/trailer.html" width="100%" height="100%"></object>';
+	}
+function opcionContactar() {
 	var a;
 	a=document.getElementById("informacion");
 	a.innerHTML='<object type="text/html" data="html/contacto.html" width="100%" height="100%"></object>';
