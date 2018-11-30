79f5fb97756b9f7aca24ef5b3b93efd1dc01590c
Yhuver <yhuver@gmail.com>
2018-11-30 00:13:31 -0500

    Login y recuperacion terminada
    Falta nosotros, trailer, contacto

---
+++ Version2/js/funciones.js
@@ -0,0 +1,20 @@
+function opcionRecuperarContraseña() {
+	var a;
+	a=document.getElementById("formularios");
+	a.innerHTML='<object type="text/html" data="html/contraseña.html" width="100%" height="100%"></object>';
+	}
+function opcionIniciarSesion() {
+	var a;
+	a=document.getElementById("formularios");
+	a.innerHTML='<object type="text/html" data="html/iniciarsesion.html" width="100%" height="100%"></object>';
+	}
+function opcion3() {
+	var a;
+	a=document.getElementById("informacion");
+	a.innerHTML='<object type="text/html" data="html/noticias.html" width="100%" height="100%"></object>';
+	}
+function opcion4() {
+	var a;
+	a=document.getElementById("informacion");
+	a.innerHTML='<object type="text/html" data="html/contacto.html" width="100%" height="100%"></object>';
+	}
